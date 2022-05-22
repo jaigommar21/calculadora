4316cc763a6c37408a600bc77edb6b865ea33505
Jaime <you@example.com>
2022-05-22 12:07:48 -0500

    Manejo de excepciones de los operadores de entrada, deben ser númericos

--- src/main/java/com/calculadora/services/CalculadoraServiceImpl.java
+++ src/main/java/com/calculadora/services/CalculadoraServiceImpl.java
@@ -34,7 +34,7 @@ public class CalculadoraServiceImpl implements CalculadoraService {
 		else if ("RESTA".equals(operador))
 			res = ope1.subtract(ope2);
 		else
-			throw new OperationNotSupportException("Operacion '" + operador + "' no soportado");
+			throw new OperationNotSupportException("Operacion '" + operador + "' no soportada");
 
 		res = res.setScale(NRO_DECIMALES, RoundingMode.HALF_UP);
 		
